8e78a615845b11181f861906677937b34c4fd63f
smavanat <edwin.hadass@hotmail.com>
2025-08-11 18:26:13 +0100

    Added support for linux

--- Destruction Engine/polypartition.cpp
+++ Destruction Engine/polypartition.cpp
@@ -21,7 +21,7 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#include "Polypartition/polypartition.h"
+#include "PolyPartition/polypartition.h"
 
 #include <math.h>
 #include <string.h>
